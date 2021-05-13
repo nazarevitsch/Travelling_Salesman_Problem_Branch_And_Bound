dacca41ed8cb517330b075796b47097b80f5a0c4
Starlight13 <o.romanishina@gmail.com>
2021-05-13 11:58:01 +0300

    added table printing

---
+++ src/main/java/binary_tree/TreePrinter.java
@@ -0,0 +1,94 @@
+package binary_tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TreePrinter {
+
+    public static void printNode(Node root) {
+        int maxLevel = TreePrinter.maxLevel(root);
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static void printNodeInternal(List<Node> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || TreePrinter.isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        TreePrinter.printWhitespaces(firstSpaces+5);
+
+        List<Node> newNodes = new ArrayList<Node>();
+        for (Node node : nodes) {
+            if (node != null) {
+                if (!node.isIncluded())
+                    System.out.print(node.getPath() + "(" + node.getSum() + ")");
+                else
+                    System.out.print("!" + node.getPath() + "(" + node.getSum() + ")");
+                newNodes.add(node.getLeft());
+                newNodes.add(node.getRight());
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            TreePrinter.printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                TreePrinter.printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    TreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).getLeft() != null)
+                    System.out.print("        /    ");
+                else
+                    TreePrinter.printWhitespaces(8);
+
+                TreePrinter.printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).getRight() != null)
+                    System.out.print("\\");
+                else
+                    TreePrinter.printWhitespaces(1);
+
+                TreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println("");
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static int maxLevel(Node node) {
+        if (node == null)
+            return 0;
+
+        return Math.max(TreePrinter.maxLevel(node.getLeft()), TreePrinter.maxLevel(node.getRight())) + 1;
+    }
+
+    private static boolean isAllElementsNull(List list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+
+        return true;
+
+    }
+}
